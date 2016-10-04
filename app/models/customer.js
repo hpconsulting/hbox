f3a70bcc3db94c07b3f7da45d6e2c6088a3dc6e8
THIMOTHEE Rudy <dathims@gmail.com>
2016-10-04 20:08:35 +0200

    retest

--- app/models/customer.js
+++ app/models/customer.js
@@ -4,15 +4,7 @@ var Schema       = mongoose.Schema;
 var CustomerSchema   = new Schema({
    id:Number,
    id_shop:Number,
-   id_shop_group:Number,
-   secure_key:String,
-   note:String,
-   id_gender:Number,
-   id_default_group:Number,
-   id_lang:Number,
-   lastname:String,
-   firstname:String,
-   email:String
+   id_shop_group:Number
 });
 
 module.exports = mongoose.model('Customer', CustomerSchema);
