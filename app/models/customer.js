d3a57bbad0e4a872fabf3deeda5d139004d780c5
THIMOTHEE Rudy <dathims@gmail.com>
2016-11-15 13:51:38 +0100

    lol

--- app/models/customer.js
+++ app/models/customer.js
@@ -2,16 +2,16 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var CustomerSchema   = new Schema({
-   id:Number,
-   id_shop:Number
+   id:Schema.Types.Mixed,
+   id_shop:Schema.Types.Mixed,
    // id_shop_group:Number,
    // secure_key:String,
    // note:String,
    // id_gender:Number,
    // id_default_group:Number,
    // id_lang:Number,
-   // lastname:String,
-   // firstname:String,
+   lastname:String,
+   firstname:String
    // email:String,
    // newsletter:Boolean,
    // ip_registration_newsletter:String,
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('dev')); // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port     = process.env.PORT || 8080; // set our port
+var port     = process.env.PORT || 3333; // set our port
 var mongobase = 'mongodb://localhost/happit';
 
 var mongoose   = require('mongoose');
